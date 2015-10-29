f4e2e239a605e9ff960e39f01bfcfe2db427e64e
小島潤 <jun.kojima.pg@gmail.com>
2015-10-29 23:06:03 +0900

    初期登録

---
+++ draw_shape.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <assert.h>
+#include "shape.h"
+
+void draw_polyline(Shape *shape);
+void draw_rectangle(Shape *shape);
+void draw_circle(Shape *shape);
+
+void draw_primitive(Shape *shape)
+{
+  switch(shape->u.primitive.type){
+    case POLYLINE_PRIMITIVE:
+      draw_polyline(shape);
+      break;
+    case RECTANGLE_PRIMITIVE:
+      draw_rectangle(shape);
+      break;
+    case CIRCLE_PRIMITIVE:
+      draw_polyline(shape);
+      break;
+    default:
+      assert(0);
+  }
+}
+
+void draw_all_shapes(Shape *head)
+{
+  Shape *pos;
+
+  for(pos = head; pos != NULL; pos = pos->next){
+    switch (pos->type){
+      case PRIMITIVE_SHAPE:
+        draw_primitive(pos);
+        break;
+      case GROUP_SHAPE:
+        draw_all_shapes(pos->u.group.head);
+        break;
+      default:
+        assert(0);
+    }
+  }
+}
