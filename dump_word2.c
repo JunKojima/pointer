a7549d791dd42a10e155110a702720871a979d25
小島潤 <jun.kojima.pg@gmail.com>
2015-10-29 22:10:27 +0900

    初期登録

---
+++ dump_word2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "word_manage_p2.h"
+
+/******************************
+ *単語の一覧をダンプする
+ ******************************/
+void dump_word(FILE *fp)
+{
+  Word *pos;
+
+  for(pos = word_header; pos; pos = pos->next){
+    fprintf(fp, "%-20s%5d\n", pos->name, pos->count);
+  }
+}
---
+++ finalize2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "word_manage_p2.h"
+
+/***********************************
+ *単語管理部の終了処理を行う
+ ***********************************/
+void word_finalize(void)
+{
+  Word *temp;
+
+  /* 登録されている単語をすべてfree()する */
+  while(word_header != NULL){
+    temp = word_header;
+    word_header = word_header->next;
+
+    free(temp->name);
+    free(temp);
+  }
+}
